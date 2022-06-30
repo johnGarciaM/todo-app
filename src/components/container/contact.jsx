77c01820d8aa0ab0faa3832a62d8e32677226133
John Garcia <john.freddy.gm@gmail.com>
2022-06-29 22:58:20 -0500

    set styles in contact component

--- src/components/container/contact.jsx
+++ src/components/container/contact.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import { useState } from "react";
 import { Contact } from "../../models/contact.class";
 import { ContactDetail } from "../pure/contactDetail";
 
 const ContactComponent = () => {
-  const newContact = new Contact({
-    name: "John",
-    lastName: "García",
-    email: "test@gmail.com",
-    connected: true,
-  });
-  return (
-    <div>
-      <h1>Información de contacto</h1>
-      <ContactDetail contact={newContact} />
-    </div>
-  );
+	// * Initialize contacts by default.
+	const contact1 = new Contact({
+		id: 1,
+		name: "John",
+		lastName: "García",
+		email: "test@gmail.com",
+		connected: true,
+	});
+	const contact2 = new Contact({
+		id: 2,
+		name: "Pablo",
+		lastName: "Ramires",
+		email: "test2@gmail.com",
+		connected: false,
+	});
+
+	let [contacts, setContacts] = useState([contact1, contact2]);
+
+	// const addContact = (contact) => {
+	// 	contacts = [...contacts, contact];
+	// 	setContacts(contacts);
+	// };
+
+	const updateContact = (id) => {
+		console.log("id :>> ", id);
+	};
+
+	const deleteContact = (id) => {
+		contacts = contacts.filter((contact) => contact.id !== id);
+		setContacts(contacts);
+	};
+
+	return (
+		<div>
+			<h1>Información de contacto</h1>
+			<ContactDetail
+				contacts={contacts}
+				updateContact={updateContact}
+				deleteContact={deleteContact}
+			/>
+		</div>
+	);
 };
 
 export default ContactComponent;
